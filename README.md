9a13be81bf222c9545c4a0810ad55e454a729fad
Alexei Tyagunov <Tyagunov217@gmail.com>
2025-04-30 19:50:39 +0300

    Проект. API для Yatube

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # api_final
-api final
+## Описание проекта
+### Это платформа для общения, где пользователи могут:
+
+- Через личный профиль публиковать свои записи (посты).
+
+- Обсуждать публикации, оставляя комментарии под постами других участников.
+
+- Подписываться на интересных авторов, чтобы следить за ними.
+
+Проект построен по принципу социальной сети с упором на текстовое взаимодействие между пользователями.
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/Alexei217/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python3 -m venv env
+```
+
+```
+source env/bin/activate
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+python3 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python3 manage.py migrate
+```
+
+Запустить проект:
+
+```
+python3 manage.py runserver
+```
+
+### Далее приведены примеры использования API:
+- ### **Публикации:**
+![1](images/post/1.jpg)
+![2](images/post/2.jpg)
+![3](images/post/3.jpg)
+![4](images/post/4.jpg)
+![5](images/post/5.jpg)
+![6](images/post/6.jpg)
+
+- ### **Комментрарии:**
+![1](images/comment/1.jpg)
+![2](images/comment/2.jpg)
+![3](images/comment/3.jpg)
+![4](images/comment/4.jpg)
+![5](images/comment/5.jpg)
+![6](images/comment/6.jpg)
+  
+- ### **Сообщества:**
+![1](images/group/1.jpg)
+
+- ### **Подписки:**
+![1](images/Follow/1.jpg)
+
+- ### **JWT-токены:**
+![1](images/jwt/1.jpg)
+![2](images/jwt/1.jpg)
+![3](images/jwt/1.jpg)
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,66 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from rest_framework.relations import SlugRelatedField
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow
+
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True, slug_field="username"
     )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Comment
+        read_only_fields = ("post",)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = "__all__"
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = "__all__"
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Follow.objects.all(), fields=("user", "following")
+            )
+        ]
+
+    def validate_following(self, value):
+        user = self.context.get("request").user
+        follow = get_object_or_404(User, username=value)
+        if user == follow:
+            raise serializers.ValidationError(
+                "Пользователь не может подписаться на себя!"
+            )
+        return value
